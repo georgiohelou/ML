86cd025f12e44ad0fdf02d399b7e578fb2d09e10
nat06 <nathan.pollet2@gmail.com>
2021-12-06 12:17:10 +0100

    features-testing

--- master_pickle.py
+++ master_pickle.py
@@ -6,21 +6,21 @@ Created on Sat Nov 27 17:35:46 2021
 @author: nathan.pollet
 """
 
-import pickle; import os
+import pickle
+import os
 
 masterlist = []
-root = '/Users/nathanpollet/ML/good_save/'
 for k in range(37):
     print(k)
-    print(f"good_save/bucket{k}.pkl")
-    with open(f"good_save/bucket{k}.pkl", 'rb') as f:
+    print(f"bucket{k}.pkl", 'rb')
+    with open(f"bucket{k}.pkl", 'rb') as f:
         bucketlist = pickle.load(f)
+        # print("len(bucketlist) : ", len(bucketlist))
         for i in bucketlist:
             masterlist.append(i)
     f.close()
-    
-print("len(masterlist) : ", len(masterlist))
+
+print("len(masterlist) = ", len(masterlist))
 print("saving masterpickle...")
-with open('fullEmbeddings.pkl', 'wb') as f1:
+with open('fullEmbeddings_random.pkl', 'wb') as f1:
     pickle.dump(masterlist, f1)
-    
